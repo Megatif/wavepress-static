2d29bd7144edacf849e442fda486db8e46c16f13
Megatif <mnr11970@hotmail.fr>
2026-04-02 19:05:03 +0100

    Update static

---
+++ 127.0.0.1/newswave/wp-content/themes/wavepress/widgets/video_grid2/js/frontend0660.js
@@ -0,0 +1,115 @@
+// Wait for the document to be ready
+jQuery(function ($) {
+
+  function load_posts(elementId, settings, page, categories) {
+    const module = "video_grid";
+
+    $.ajax({
+      type: "POST",
+      url: my_ajax_object.ajax_url + "/wp-admin/admin-ajax.php",
+      dataType: "html", // add data type
+      data: {
+        action: "wavepress_get_ajax_posts",
+        postsPerPage: settings.postsPerPage,
+        categories: categories,
+        display_author: settings.display_author,
+        settings: settings,
+        page: page,
+        module: module,
+      },
+      beforeSend: function () {
+        $("#" + elementId + " #overlay").fadeIn(300);
+        $("#" + elementId).animate({ opacity: 0.5 }, 500);
+      },
+      success: function ( data ) {
+
+        parser = new DOMParser();
+        doc = parser.parseFromString( data.html ,"text/html" );
+        const principal_article = doc.getElementsByClassName("c0")[0];
+        const secondary_articles = Object.values(
+          doc.getElementsByClassName("c1"),
+        );
+        let pagination = doc.getElementsByClassName("pagination-list")[0];
+        
+        $(
+          "#" + elementId + " div.section-content div.primary-article-wrapper",
+        ).html(principal_article);
+
+        $(
+          "#" +
+            elementId +
+            " div.section-content div.secondary-article-wrapper",
+        ).html("");
+
+        secondary_articles.forEach(function (elem) {
+          $(
+            "#" +
+              elementId +
+              " div.section-content div.secondary-article-wrapper",
+          ).append(elem);
+        });
+        
+        // Set pagination
+        $("#" + elementId + " .posts-pagination").html(
+          pagination == null ? "" : pagination,
+        );
+
+        $(
+          "#" +
+            elementId +
+            " .secondary-article-title a,#" +
+            elementId +
+            " .primary-article-title a",
+        ).each(function () {
+          var len = $(this).text().length;
+          if (len > 60) {
+            $(this).text($(this).text().substr(0, 60) + "...  ");
+          }
+        });
+
+        $("#" + elementId + " #overlay").fadeOut(300);
+        $("#" + elementId).animate({ opacity: 1 }, 500);
+      },
+    });
+  }
+  $(".video_grid").each(function () {
+    const element = $(this);
+    const elementId = $(this).attr("id");
+    const settings = element.data("settings");
+    let page = 1;
+
+    $("#" + elementId + " ul.sub_categories_list li:first-child a").addClass("highlighted");
+    
+    // Load posts by Categories
+    $("#" + elementId + " ul.sub_categories_list li a").on(
+      "click",
+      function () {
+        
+        let categories;
+
+        categories = $(this).attr("data-catslug");
+        if(categories == 'all') {
+          categories = settings.categories;
+        }
+
+        page = 1;
+        $("#" + elementId + " ul.sub_categories_list li a").removeClass("highlighted");
+        $(this).addClass("highlighted");
+        
+        load_posts(elementId, settings, page, categories);
+      },
+    );
+    
+    // Load posts by page
+    $("#" + elementId).on("click", "ul.pagination-list li a", function () {
+      page = $(this).attr("data-page");
+      let categories;
+
+      categories = $(this).attr("data-catslug");
+      if(categories == 'all') {
+        categories = settings.categories;
+      }
+      load_posts(elementId, settings, page, categories);
+    });
+  });
+});
